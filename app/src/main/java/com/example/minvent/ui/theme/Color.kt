20b58d572b4aaa2f34e692f06ce0cebdf01773d6
Javier Hurtado <javier.hurtado0815@gmail.com>
2026-09-13 00:09:50 -0300

    sumativa 2, ajuste tema

--- app/src/main/java/com/example/minvent/ui/theme/Color.kt
+++ app/src/main/java/com/example/minvent/ui/theme/Color.kt
@@ -2,25 +2,19 @@ package com.example.minvent.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
 
-val MinLPrimary = Color(0xFF1E88E5)
-val MinLSecondary = Color(0xFF26A69A)
-val MinLBackground = Color(0xFFF0F4F8)
+val MinLPrimary = Color(0xFF2E7D32)
+val MinLSecondary = Color(0xFF00796B)
+val MinLBackground = Color(0xFFEBF5EE)
 val MinLSurface = Color(0xFFFFFFFF)
 val MinLOnPrimary = Color(0xFFFFFFFF)
-val MinLTextMain = Color(0xFF212121)
+val MinLTextMain = Color(0xFF1B2E1D)
 
 
-val MinDPrimary = Color(0xFF64B5F6)
+val MinDPrimary = Color(0xFF81C784)
 val MinDSecondary = Color(0xFF4DB6AC)
-val MinDBackground = Color(0xFF121212)
-val MinDSurface = Color(0xFF1E1E1E)
-val MinDOnPrimary = Color(0xFF0D3B66)
-val MinDTextMain = Color(0xFFE0E0E0)
+val MinDBackground = Color(0xFF111712)
+val MinDSurface = Color(0xFF1A241C)
+val MinDOnPrimary = Color(0xFF0A210E)
+val MinDTextMain = Color(0xFFE2E8E3)
